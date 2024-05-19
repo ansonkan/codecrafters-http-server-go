133a26bf72cd103162eec543e307fea310afb550
ansonkan <48442757+ansonkan@users.noreply.github.com>
2024-05-19 21:00:23 +0800

    test

--- app/server.go
+++ app/server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -19,6 +21,37 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	buf := make([]byte, 1024)
+	tmp := make([]byte, 0, 1024)
+
+	for {
+		n, err := conn.Read(buf)
+		tmp = append(tmp, buf[:n]...)
+
+		if n < len(buf) {
+			break
+		}
+
+		if err != nil {
+			if err != io.EOF {
+				fmt.Printf("Read error: %v\n", err)
+			}
+			break
+		}
+
+	}
+
+	req_parts := strings.Split(string(tmp), "\r\n")
+	line_parts := strings.Split(req_parts[0], " ")
+
+	method := line_parts[0]
+	target := line_parts[1]
+
+	if method == "GET" && target == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+
 	conn.Close()
 }
